fa3eb62a36023f0f154b7a3db9967bcb257c7ff5
StevenVroman <32570706+StevenVroman@users.noreply.github.com>
2019-12-11 12:02:08 +0100

    asd

---
+++ Frontend/src/app.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import './scss/style.scss';
+
+
+
---
+++ Frontend/webpack.config.js
@@ -0,0 +1,99 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = function(){
+  return {
+    mode: 'development',
+    entry: [
+      './src/app.js'
+    ],
+    watch: true,
+    watchOptions: {
+      aggregateTimeout: 300, // Process all changes which happened in this time into one rebuild
+      poll: 1000, // Check for changes every second,
+      ignored: /node_modules/,
+      // ignored: [
+      //   '**/*.scss', '/node_modules/'
+      // ]
+    },
+    devtool: 'source-maps',
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      watchContentBase: true,
+      hot: true,
+      open: true,
+      inline: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack starter project',
+        template: path.resolve('./src/index.html')
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [{
+            loader: 'style-loader', // inject CSS to page
+          }, {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+          }, {
+            loader: 'postcss-loader', // Run postcss actions
+            options: {
+              plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: 'images/',
+                name: "[name].[ext]",
+                esModule: false,
+              },
+            },
+          ]
+        },
+        {
+          test: /\.(woff|woff2|ttf|otf|eot)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "fonts/[name].[ext]",
+              }
+            }
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+          }
+        },
+      ]
+    }
+  };
+}
