238f05601e8b12d70e683d5a5807b4064aa75448
StevenVroman <32570706+StevenVroman@users.noreply.github.com>
2020-01-20 13:45:00 +0100

    Revert "ads"
    This reverts commit 370b6c62c2c54ee9ce153777d88444baf1a781e5.

--- Frontend/src/js/index.js
+++ Frontend/src/js/index.js
@@ -1,19 +1,11 @@
 
 $( document ).ready(function() {
     $( function() {
-    var w = $(window).width();
-        if (w < 768) {
-            //wanner mobile collapsed
-            $('.panel-collapse').removeClass('show');
-            $('.panel-collapse').addClass('collapse');
-        }
-        });
-        $( function() {
-            $( "#slider-range" ).slider({
-                range: true,
-                min: 599,
-                max: 10000,
-                values: [ 600, 6000 ],
+        $( "#slider-range" ).slider({
+            range: true,
+            min: 599,
+            max: 10000,
+            values: [ 600, 6000 ],
             slide: function( event, ui ) {
                 $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
             }
